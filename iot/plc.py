f6944500ceaa02e7decc3f35453a3dbeaf5a7957
Daniel Olarte <dan_olarte@hotmail.com>
2022-04-19 16:05:30 -0500

    fixed transforms

--- iot/plc.py
+++ iot/plc.py
@@ -1,9 +1,20 @@
+
+import pycomm3
+
+from pycomm3 import LogixDriver
+
+with LogixDriver('192.168.58.156') as plc:
+    print(plc)
+
+
+'''
 import pylogix
 
 from pylogix import PLC
 
 with PLC() as comm:
-    comm.IPAddress = '192.168.0.156'
-    ret = comm.Read('CONTADOR') # MI0 contador
+    comm.IPAddress = '192.168.58.156'
+    ret = comm.Read('Start') # MI0 contador
      
     print(ret.TagName, ret.Status, ret.Value)
+'''
